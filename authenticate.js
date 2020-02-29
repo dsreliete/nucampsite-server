5a17acc7fc8f70de155b65a5b1029d7e16a2c27d
dsreliete <dsreliete@gmail.com>
2020-02-29 11:42:26 -0500

    ädded new midleware to fcbk auth

--- authenticate.js
+++ authenticate.js
@@ -5,7 +5,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy; 
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-
+const FacebookTokenStrategy = require('passport-facebook-token');
 const User = require('./models/user');
 const conf = require('./config');
 
@@ -62,3 +62,36 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: conf.facebook.clientId,
+            clientSecret: conf.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.admin = true;
+                    console.log(user.firstname, user.admin)
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     // 'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-    'mongoUrl' : 'mongodb://root:rootpassword@nucamp-mongo:27017/nucampsite'
+    'mongoUrl' : 'mongodb://root:rootpassword@nucamp-mongo:27017/nucampsite',
+    'facebook': {
+        clientId: '1423496051143243',
+        clientSecret: 'feb12d7f84d2d094d2acf230635c869e'
+    }
 }
