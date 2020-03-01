deac7715d25e10a55bd8ab0452a181bf179e0e25
dsreliete <dsreliete@gmail.com>
2020-03-01 17:17:02 -0500

    üpdate favorite route

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -86,14 +86,72 @@ favoriteRouter.route('/:campsiteId')
     res.end(`GET operation not supported on /favorites/${req.params.campsiteId}`);
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-
+    Favorite.findOne({'user': req.user._id})
+    .then(campsite => {
+        if(campsite){
+            if (campsite.campsites.includes(req.params.campsiteId)){
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(`This campsite has already included`);
+            } else {
+                campsite.campsites.push(req.params.campsiteId)
+                campsite.save()
+                .then(elem => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(elem);
+                })
+                .catch(err => next(err));
+            }
+        } else {
+            const favoriteCampsite = new Favorite({user: req.user._id, campsites: req.params.campsiteId});
+            Favorite.create(favoriteCampsite)
+            .then(favCampsite => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(favCampsite);
+            })
+            .catch(err => next(err));
+        }
+    })
+    .catch(err => next(err));
 })
 .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`PUT operation not supported on /favorites/${req.params.campsiteId}`);
 })
 .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-
+    Favorite.findOne({'user': req.user._id})
+    .then(campsite => {
+        if(campsite) {
+            if (campsite.campsites.includes(req.params.campsiteId)) {
+                const campsiteIndex = campsite.campsites.indexOf(req.params.campsiteId);
+                const splicedCampsite = campsite.campsites.splice(campsiteIndex, 1);
+                campsite.save()
+                .then(camp => {
+                    if(camp.campsites.length === 0){
+                        Favorite.findByIdAndDelete(camp._id)
+                        .then(response => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(response);
+                        })
+                        .catch(err => next(err));
+                    } else {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json(camp);
+                    }
+                })
+                .catch(err => next(err));
+            } 
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(`Is not possible to delete the campsite ${req.params.campsiteId}`);
+        }
+    })
+    .catch(err => next(err));
 });
 
 module.exports = favoriteRouter;
